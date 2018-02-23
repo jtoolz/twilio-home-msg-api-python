7ac8d6abecc53b6168de4532f9e28f18abe02445
Joe Tullio <tullio@gmail.com>
2018-02-23 14:43:25 -0800

    got basic whitelist working

--- main.py
+++ main.py
@@ -14,18 +14,46 @@
 
 import webapp2
 import os
+import logging
+import json
 
 from flask import Flask, request
 from twilio.twiml.messaging_response import Message, MessagingResponse
 from twilio.twiml.voice_response import VoiceResponse
 
+from google.appengine.ext import ndb
+
 # Config settings
 TWILIO_PHONE_NUM = os.environ['TWILIO_PHONE_NUM']
 TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 TWILIO_ACCT_SID = os.environ['TWILIO_ACCT_SID']
+WHITELIST_FILE = os.environ['WHITELIST_FILE']
+DEFAULT_MESSAGELIST_NAME = "default_messagelist"
 # End config
 
+whitelist = {"+15555551212":"Test1", "+15555551212":"Test2"}
+
 app = Flask(__name__)
+
+def load_whitelist():
+	logging.info("loading whitelist() from " + WHITELIST_FILE)
+	f = open(WHITELIST_FILE)
+	logging.info("f is" + str(f))
+	return json.load(f)
+
+# We set a parent key to ensure they are all in the same entity
+# group. Queries across the entity group will be consistent.
+def message_key(messagelist_name = DEFAULT_MESSAGELIST_NAME):
+	return ndb.Key('MessageList', messagelist_name)
+
+class Sender(ndb.Model):
+	name = ndb.StringProperty(indexed=False)
+	phone_num = ndb.StringProperty()
+
+class HomeMessage(ndb.Model):
+	sender = ndb.StructuredProperty(Sender)
+	content = ndb.StringProperty(indexed=False)
+	timestamp = ndb.DateTimeProperty(auto_now_add=True)
 
 @app.route('/call/receive', methods=['POST'])
 def receive_voice():
@@ -38,8 +66,21 @@ def receive_voice():
 # Only accept from known numbers in address book 
 @app.route('/sms/receive', methods=['POST'])
 def receive_sms():
-    sender = request.values.get('From')
-    body = request.values.get('Body')
+    sender = str(request.values.get('From'))
+    body = str(request.values.get('Body'))
+    logging.info("received msg " + str(body) + " from " + str(sender))
+#    if (any(whitelist.values()) is False):
+#    	load_whitelist()
+
+    logging.info("whitelist is " + str(whitelist))
+    
+    if (sender in whitelist.keys()):
+    	logging.info("found key " + sender + " in whitelist")
+        homemsg = HomeMessage(parent=message_key())
+        homemsg.sender = Sender(name=whitelist[sender], phone_num=sender)
+        homemsg.content = body
+        logging.info("set body " + str(body))
+        homemsg.put()
 
     message = 'Hello, {}, you said: {}'.format(sender, body)
 
@@ -49,6 +90,7 @@ def receive_sms():
     
 
 if __name__ == '__main__':
+    
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
